d1520402f5fdd715dad9f2a749977580fa24b517
Ankit Mithbavkar <72635696+AnkhM@users.noreply.github.com>
2025-08-09 13:40:05 -0400

    Server setup

---
+++ simple-server-setup/server.py
@@ -0,0 +1,29 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+# Create MCP Server
+mcp = FastMCP(
+    name="Calculator",
+    host="0.0.0.0", # Used for SSE transport (localhost)
+    port = 6274 # Used for SSE transport
+)
+
+# Add a simple calculator tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    return a + b
+
+# Run the server
+if __name__ == "__main__":
+    transport = "sse"
+    if transport == "stdio":
+        print("Running server with stdio transport")
+        mcp.run(transport="stdio")
+    elif transport == "sse":
+        print("Running server with SSE transport")
+        mcp.run(transport="sse")
+    else:
+        ValueError(f"Unknown transport: {transport}")
+
